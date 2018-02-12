016e2142871465f32177a223f6c40b88d25650c8
starlying <corp@siteserver.cn>
2018-02-12 10:11:01 +0800

    docs

--- gulpfile.js
+++ gulpfile.js
@@ -2,31 +2,36 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 var rename = require("gulp-rename");
 var minify = require('gulp-minifier');
 var replace = require('gulp-replace');
+var runSequence = require('run-sequence');
+var del = require('del');
 
-gulp.task('build', function () {
-  var version = process.env.APPVEYOR_BUILD_VERSION;
+gulp.task('build-clean', function () {
+  return del(['./dist']);
+});
 
-  console.log('build SiteServer.UI started, version: ' + version);
+gulp.task('build-css', function () {
+  return gulp.src('./src/*.scss').pipe(sourcemaps.init()).pipe(sass().on('error', sass.logError)).pipe(sourcemaps.write()).pipe(gulp.dest('./dist'));
+});
 
-  gulp.src('./src/scss/*.scss').pipe(sass().on('error', sass.logError)).pipe(gulp.dest('./src/css'));
-  gulp.src('./src/css/siteserver.css').pipe(gulp.dest('./dist/css'));
-  gulp.src('./src/css/siteserver.css').pipe(minify({
+gulp.task('build-min', function () {
+  return gulp.src('./dist/siteserver.css').pipe(minify({
     minify: true,
     collapseWhitespace: true,
     conservativeCollapse: true,
     minifyJS: false,
     minifyCSS: true,
     minifyHTML: false
-  })).pipe(rename('siteserver.min.css')).pipe(gulp.dest('./dist/css'));
+  })).pipe(rename('siteserver.min.css')).pipe(gulp.dest('./dist'));
+});
 
-  gulp.src('./src/fonts/**').pipe(gulp.dest('./dist/fonts'));
+gulp.task('build-docs', function () {
+  gulp.src('./dist/**').pipe(gulp.dest('./docs/assets/css'));
+});
 
-  gulp.src('./src/lib/**').pipe(gulp.dest('./dist/css'));
-
-  gulp.src('./package.json').pipe(replace('$version$', version)).pipe(gulp.dest('./'));
-
-  console.log('build SiteServer.UI successed!');
+gulp.task('build', function (callback) {
+  runSequence('build-clean', 'build-css', 'build-min', 'build-docs', callback);
 });
